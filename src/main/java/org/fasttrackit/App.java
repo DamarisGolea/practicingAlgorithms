6d6553b71b74106e99e05798b7df3e8011a1a42e
Your Name <damaris.golea@gmail.com>
2019-06-12 21:28:03 +0300

    practicing methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public void displayAverage(int num1, int num2, int num3) {
         System.out.println("Calculating average of: " + num1 + ", " + num2 + ", " + num3 + ". ");
-        double average = (num1 + num2 + num3) / 3;
+        double average = (num1 + num2 + num3) / 3d;
         System.out.println("Average:" + average);
     }
 
