99bb81213e194c502e0b219a171b9e799e92851d
Your Name <damaris.golea@gmail.com>
2019-06-12 21:25:33 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,22 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public void displayAverage(int num1, int num2, int num3) {
+        System.out.println("Calculating average of: " + num1 + ", " + num2 + ", " + num3 + ". ");
+        double average = (num1 + num2 + num3) / 3;
+        System.out.println("Average:" + average);
+    }
+
+
+    public static void main(String[] args) {
+
+        App app = new App();
+        app.displayAverage(19, 20, 98);
+    }
+}
+
