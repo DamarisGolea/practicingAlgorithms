5a1230834344bc637a5a5e55a5ce3fb0c309cf4d
Your Name <damaris.golea@gmail.com>
2019-07-17 20:13:41 +0300

    SortingRecords

---
+++ src/main/java/org/fasttrackit/sortingrecords/Record.java
@@ -0,0 +1,31 @@
+package org.fasttrackit.sortingrecords;
+
+public class Record {
+
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/org/fasttrackit/sortingrecords/SortingRecords.java
@@ -0,0 +1,25 @@
+package org.fasttrackit.sortingrecords;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortingRecords {
+    public static void main(String[] args) {
+        Record record1 = new Record();
+        record1.setFirstName("Ana");
+        record1.setLastName("Pop");
+
+        Record record2 = new Record();
+        record2.setFirstName("Bogdan");
+        record2.setLastName("Muresan");
+
+        List<Record> records = Arrays.asList(record1, record2);
+
+        Comparator<Record> lastNameComparator = Comparator.comparing(Record::getLastName);
+        records.sort(lastNameComparator);
+
+        System.out.println(records);
+    }
+
+}
